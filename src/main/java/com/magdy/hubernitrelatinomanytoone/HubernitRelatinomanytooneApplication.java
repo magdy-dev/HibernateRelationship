1a2c3ab8a57ebaf9aeb92696d600021f79dbb60a
magdy <magdyshenoda2021@gmail.com>
2023-08-07 14:25:54 +0300

    HubernitRelatinomanytooneApplication

---
+++ src/main/java/com/magdy/hubernitrelatinomanytoone/HubernitRelatinomanytooneApplication.java
@@ -0,0 +1,13 @@
+package com.magdy.hubernitrelatinomanytoone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubernitRelatinomanytooneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubernitRelatinomanytooneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magdy/hubernitrelatinomanytoone/HubernitRelatinomanytooneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magdy.hubernitrelatinomanytoone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubernitRelatinomanytooneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
